efb4c2f53c56b1833b8adce65739297728172f96
Shirley <scao8@students.d125.org>
2016-05-10 10:54:28 -0500

    Option

--- MashGame/Option.java
+++ MashGame/Option.java
@@ -1,33 +1,16 @@
-
-/**
- * Write a description of class Option here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class Option
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Option
-     */
-    public Option()
+    private String option = "";
+    public Option(String o)
     {
-        // initialise instance variables
-        x = 0;
+        option = o;
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
+    public String getOption()
     {
-        // put your code here
-        return x + y;
+        return option;
+    }
+    public void setOption(String o)
+    {
+        option = o;
     }
 }
