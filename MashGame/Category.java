f578acc49958175d50b8b87f0d50634283dfcbbd
Shirley <scao8@students.d125.org>
2016-05-10 10:40:50 -0500

    Changes

---
+++ MashGame/Category.java
@@ -0,0 +1,15 @@
+public class Category
+{
+    private String name;
+    public Category()
+    {
+    }
+    public String getCategory()
+    {
+        return name;
+    }
+    public void setCategory(String s)
+    {
+        name=s;
+    }
+}
